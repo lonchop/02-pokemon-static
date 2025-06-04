b492ece95506e6a0f1e8056d21bd546275de3648
lonchop <goranjose16@gmail.com>
2025-06-04 17:33:14 -0400

    # Integración de Vue y mejoras en la navegación
    Se ha añadido la integración de `@astrojs/vue` para permitir el uso de componentes Vue en la aplicación. Además, se han realizado mejoras en la navegación, incluyendo la adición de un nuevo enlace a "Islands" en el componente `Navbar` y ajustes en el diseño de los botones de navegación.
    
    ## Cambios realizados:
    
    - Se ha importado `@astrojs/vue` en `astro.config.mjs` y se ha añadido como dependencia en `package.json`.
    - Se ha modificado el componente `Navbar` para incluir un nuevo enlace a "Islands".
    - Se han realizado ajustes en el diseño de los botones de navegación en la página de Pokemons para mejorar la presentación.
    
    ### Archivos modificados:
    
    - astro.config.mjs
    - package.json
    - package-lock.json
    - src/components/shared/Navbar.astro
    - src/pages/pokemons/[name].astro
    - src/pages/pokemons/[page].astro
    - tsconfig.json

---
+++ src/components/CounterVue.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const decrement = () => {
+  count.value--;
+};
+
+const increment = () => {
+  count.value++;
+};
+
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold">Counter</h1>
+  <p class="text-2xl">Count: {{ count }}</p>
+
+  <div class="flex flex-row gap-2">
+    <button class="bg-blue-500 cursor-pointer text-white p-2 rounded-md w-10" @click="decrement">-1</button>
+    <button class="bg-blue-500 cursor-pointer text-white p-2 rounded-md w-10" @click="increment">+1</button>
+  </div>
+</template>
+
